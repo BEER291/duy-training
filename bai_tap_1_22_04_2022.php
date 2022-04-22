bf91837afd23cfbbd240853295c99ed18cac82b9
BEER291 <104116445+BEER291@users.noreply.github.com>
2022-04-23 01:05:35 +0700

    Update bai_tap_1_22_04_2022.php

--- bai_tap_1_22_04_2022.php
+++ bai_tap_1_22_04_2022.php
@@ -2,12 +2,12 @@
     $n = 5;
     $x = 2;
     $y = -1;
-    $test = array($x, $y);
-    for ($i = 0; $i <= $n; $i++) {
+    $test = array();
+    for ($i = 0; $i < $n; $i++) {
         $u = $i % 2;
         if ($u == 0) {
-            $x = $x - 0.5;
             $test[] = $x;
+            $x = $x - 0.5;
         }
         else {
             $test[] = $y;
