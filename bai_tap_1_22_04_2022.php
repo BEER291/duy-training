2477410234c9522b3a28c64f2ae88bd93f9345e2
BEER291 <104116445+BEER291@users.noreply.github.com>
2022-04-23 01:18:21 +0700

    Create bai_tap_1_22_04_2022.php

---
+++ bai_tap_1_22_04_2022.php
@@ -0,0 +1,16 @@
+<?php
+    $n = 5;
+    $x = 2;
+    $test = array();
+    for ($i = 0; $i < $n; $i++) {
+        $u = $i % 2;
+        if ($u == 0) {
+            $test[] = $x;
+            $x = $x - 0.5;
+        }
+        else {
+            $test[] = -1;
+        }
+    }
+    echo "$test[0]<br>$test[1]<br>$test[2]<br>$test[3]<br>$test[4]";
+?>
