aac6b044cf1a0204d0407170505fc3ca326318c2
BEER291 <104116445+BEER291@users.noreply.github.com>
2022-04-28 11:25:24 +0700

    Update test.php

--- bai_tap_8_28_04_2022/test.php
+++ bai_tap_8_28_04_2022/test.php
@@ -19,10 +19,24 @@ if (!$ket_qua) {
     exit();
 }
 
+function myFunction ($category_name) {
+    $array = explode(',', $category_name);
+    for ($i = 0; $i < count($array); $i++ ) {
+        if ($array[$i] == 1) {
+            $array[$i] = "PC GVN GAMING";
+        }
+        else {
+            $array[$i] = "PC GVN VĂN PHÒNG";
+        }
+    }
+    $string = implode(", ", $array);
+    echo $string;
+}
+
 while ($row = $ket_qua->fetch_array(MYSQLI_ASSOC)) {
-    echo "<p>ID: " . $row['id'] . "</p>";
     echo "<p>SKU: " . $row['sku'] . "</p>";
-    echo "<p>Danh mục: " . $row['category_ids'] . "</p>";
+    echo "<p>Danh mục: " . "</p>";
+    myFunction ($row['category_ids']);
     echo "<p>Tên sản phẩm: " . $row['name'] . "</p>";
     echo "<p>Mô tả: " . $row['description'] . "</p>";
     echo "<p>Hình ảnh: </p>";
