656372daa5709db9b42c9cfc660a9ff02c5194d5
BEER291 <104116445+BEER291@users.noreply.github.com>
2022-04-29 15:55:13 +0700

    Update test.php

--- bai_tap_8_28_04_2022/test.php
+++ bai_tap_8_28_04_2022/test.php
@@ -19,30 +19,15 @@ if (!$ket_qua) {
     exit();
 }
 
-function myFunction($category_name) {
-    $array = explode(',', $category_name);
-    $username = "user_demo";
-    $password = "123456";
-    $server   = "localhost";
-    $dbname   = "demo";
-
-$connect = new mysqli($server, $username, $password, $dbname);
-    for ($i = 0; $i < count($array); $i++) {
-        $sql2 = "SELECT name FROM danhmuc WHERE id = $array[$i]";
-        $ket_qua2 = $connect->query($sql2);
-        $row = $ket_qua2->fetch_array(MYSQLI_ASSOC);
-        foreach ($row as $value) {
-            $array2[] = $value;
-        }
-    }
-    $string = implode(", ", $array2);
-    echo $string;
-}
-
 while ($row = $ket_qua->fetch_array(MYSQLI_ASSOC)) {
     echo "<p>SKU: " . $row['sku'] . "</p>";
-    echo "<p>Danh mục: " . "</p>";
-    myFunction ($row['category_ids']);
+    echo "<p>Danh mục: </p>";
+    $array=array_map('intval', explode(',', $row['category_ids']));
+    $array = implode("','",$array);
+    $query=mysqli_query($connect, "SELECT name FROM danhmuc WHERE id IN ('".$array."')");
+    while ($row2 = $query->fetch_array(MYSQLI_ASSOC)) {
+        echo $row2['name'] . "<br>";
+    }
     echo "<p>Tên sản phẩm: " . $row['name'] . "</p>";
     echo "<p>Mô tả: " . $row['description'] . "</p>";
     echo "<p>Hình ảnh: </p>";
